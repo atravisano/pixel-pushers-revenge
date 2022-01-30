7dc0fc3433ab5c57a162bf0f1a6e249312e0e47f
Steven Hook <steven@hookscode.com>
2022-01-30 00:24:35 -0500

    score and deck reset

--- Assets/Scripts/Mechanics/KinematicObject.cs
+++ Assets/Scripts/Mechanics/KinematicObject.cs
@@ -174,7 +174,6 @@ namespace Platformer.Mechanics
                         }
                         if (GetType() == typeof(PlayerController))
                         {
-                            Debug.Log("Hello");
                         }
                     }
 
